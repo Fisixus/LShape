cbd7d4047f16f56ef3939c674ebb5881851114a6
Fisixus <31778258+Fisixus@users.noreply.github.com>
2020-02-02 13:24:51 +0300

    LShape has been drawn.

--- GameManager.cpp
+++ GameManager.cpp
@@ -4,81 +4,68 @@
 typedef Angel::vec4  color4;
 typedef Angel::vec4  point4;
 
-const int NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
+const int NumVertices = 12; // 6 vertex
+GLuint ww = 500;
+GLuint wh = 500;
 
 point4 points[NumVertices];
 color4 colors[NumVertices];
 
-// Vertices of a unit cube centered at origin, sides aligned with axes
-point4 vertices[8] = {
-	point4(-0.5, -0.5,  0.5, 1.0),
-	point4(-0.5,  0.5,  0.5, 1.0),
-	point4(0.5,  0.5,  0.5, 1.0),
-	point4(0.5, -0.5,  0.5, 1.0),
-	point4(-0.5, -0.5, -0.5, 1.0),
-	point4(-0.5,  0.5, -0.5, 1.0),
-	point4(0.5,  0.5, -0.5, 1.0),
-	point4(0.5, -0.5, -0.5, 1.0)
+point4 LShape[6] = {
+	point4(-1.0,-1.0 ,0.0, 1.0), //a0
+	point4(-1.0,-0.6 ,0.0, 1.0), //a1
+	point4(-0.9,-0.6 ,0.0, 1.0), //a2
+	point4(-0.9,-0.9 ,0.0, 1.0), //a3
+	point4(-0.8,-0.9 ,0.0, 1.0), //a4
+	point4(-0.8,-1.0 ,0.0, 1.0), //a5
 };
 
 // RGBA colors
-color4 vertex_colors[8] = {
+color4 vertex_colors[6] = {
 	color4(0.0, 0.0, 0.0, 1.0),  // black
 	color4(1.0, 0.0, 0.0, 1.0),  // red
 	color4(1.0, 1.0, 0.0, 1.0),  // yellow
 	color4(0.0, 1.0, 0.0, 1.0),  // green
 	color4(0.0, 0.0, 1.0, 1.0),  // blue
 	color4(1.0, 0.0, 1.0, 1.0),  // magenta
-	color4(1.0, 1.0, 1.0, 1.0),  // white
-	color4(0.0, 1.0, 1.0, 1.0)   // cyan
 };
 
-// Array of rotation angles (in degrees) for each coordinate axis
-enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
-int      Axis = Xaxis;
-GLfloat  Theta[NumAxes] = { 30.0, 30.0, 0.0 };
-
-GLuint  theta;  // The location of the "theta" shader uniform variable
+point4 referencePoint = point4(-0.9, -0.9, 0.0, 1.0);
+GLuint  theta = 5;  // The location of the "theta" shader uniform variable
 
 //----------------------------------------------------------------------------
 
 // quad generates two triangles for each face and assigns colors
 //    to the vertices
 int Index = 0;
-void
-quad(int a, int b, int c, int d)
+void quad(int a, int b, int c, int d)
 {
-	colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-	colors[Index] = vertex_colors[b]; points[Index] = vertices[b]; Index++;
-	colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
-	colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-	colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
-	colors[Index] = vertex_colors[d]; points[Index] = vertices[d]; Index++;
+	colors[Index] = vertex_colors[a]; points[Index] = LShape[a]; Index++;
+	colors[Index] = vertex_colors[b]; points[Index] = LShape[b]; Index++;
+	colors[Index] = vertex_colors[c]; points[Index] = LShape[c]; Index++;
+	colors[Index] = vertex_colors[a]; points[Index] = LShape[a]; Index++;
+	colors[Index] = vertex_colors[c]; points[Index] = LShape[c]; Index++;
+	colors[Index] = vertex_colors[d]; points[Index] = LShape[d]; Index++;
 }
 
 //----------------------------------------------------------------------------
 
-// generate 12 triangles: 36 vertices and 36 colors
-void
-colorcube()
+void fillColorsandPoints()
 {
-	quad(1, 0, 3, 2);
-	quad(2, 3, 7, 6);
-	quad(3, 0, 4, 7);
-	quad(6, 5, 1, 2);
-	quad(4, 5, 6, 7);
-	quad(5, 4, 0, 1);
+	quad(0, 1, 2, 3);
+	quad(3, 4, 5, 0);
 }
 
 //----------------------------------------------------------------------------
+
 
 // OpenGL initialization
-void
-init()
+void init()
 {
-	colorcube();
+	fillColorsandPoints();
 
-	printf("Pressing left, middle and right  mouse buttons will change the rotation axis to X, Y, and Z, respectively.\n");;
+	//printf("Pressing left, middle and right  mouse buttons will change the rotation axis to X, Y, and Z, respectively.\n");;
+
 	// Create a vertex array object
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
@@ -94,7 +81,7 @@ init()
 	glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
 
 	// Load shaders and use the resulting shader program
-	GLuint program = InitShader("vshader36.glsl", "fshader36.glsl");
+	GLuint program = InitShader("vshader.glsl", "fshader.glsl");
 	glUseProgram(program);
 
 	// set up vertex arrays
@@ -116,12 +103,11 @@ init()
 
 //----------------------------------------------------------------------------
 
-void
-display(void)
+void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glUniform3fv(theta, 1, Theta);
+	//glUniform3fv(theta, 1, Theta);
 	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 
 	glutSwapBuffers();
@@ -129,8 +115,7 @@ display(void)
 
 //----------------------------------------------------------------------------
 
-void
-keyboard(unsigned char key, int x, int y)
+void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 	case 033: // Escape Key
@@ -142,52 +127,52 @@ keyboard(unsigned char key, int x, int y)
 
 //----------------------------------------------------------------------------
 
-void
-mouse(int button, int state, int x, int y)
+void mouse(int button, int state, int x, int y)
 {
 	if (state == GLUT_DOWN) {
 		switch (button) {
-		case GLUT_LEFT_BUTTON:    Axis = Xaxis;  break;
-		case GLUT_MIDDLE_BUTTON:  Axis = Yaxis;  break;
-		case GLUT_RIGHT_BUTTON:   Axis = Zaxis;  break;
+		case GLUT_LEFT_BUTTON:     break;
+		case GLUT_MIDDLE_BUTTON:   break;
+		case GLUT_RIGHT_BUTTON:    break;
 		}
 	}
 }
 
-//----------------------------------------------------------------------------
-
-void
-idle(void)
+void reshapeFunc(GLsizei w, GLsizei h)
 {
-	Theta[Axis] += 0.5;
+	/* adjust clipping box */
 
-	if (Theta[Axis] > 360.0) {
-		Theta[Axis] -= 360.0;
-	}
 
+	/* adjust viewport and clear */
+
+	glViewport(0, 0, ww*h / wh, h);
+
+	/* set global size for use by drawing routine */
+
+	//ww = w;
+	//wh = h;
+	//resetGame();
 	glutPostRedisplay();
 }
-
 //----------------------------------------------------------------------------
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(512, 512);
+	glutInitWindowSize(ww, wh);
 
 	// set OpenGL context to 3.1 or 3.2 and 
 	// set profile to core
 	// deprecated functions will not be available in core profile
 	// the other option is GLUT_COMPATIBILITY PROFILE which let's us use deprecated functionality if
 	// vendor implementation provide it
-	glutInitContextVersion(3, 1);
-	printf("OpenGL version is (%s)\n", glGetString(GL_VERSION));
-	glutInitContextProfile(GLUT_CORE_PROFILE);
+
+	//glutInitContextVersion(3, 1);
+	//glutInitContextProfile(GLUT_CORE_PROFILE);
 
 	//create graphics window
-	glutCreateWindow("Color Cube");
+	glutCreateWindow("L Shape");
 
 	//include the following statement due to an error in GLEW library
 	glewExperimental = GL_TRUE;
@@ -195,11 +180,10 @@ main(int argc, char **argv)
 	glewInit();
 
 	init();
-
+	glutReshapeFunc(reshapeFunc);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouse);
-	glutIdleFunc(idle);
 
 	glutMainLoop();
 	return 0;
